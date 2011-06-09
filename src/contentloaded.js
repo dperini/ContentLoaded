df018edee9c609f4dde59fbfac0f982d2997419e
Johan Sundström <oyasumi+github@gmail.com>
2011-06-08 18:13:16 -0700

    A little tidier.

--- src/contentloaded.js
+++ src/contentloaded.js
@@ -5,7 +5,7 @@
  * Summary: cross-browser wrapper for DOMContentLoaded
  * Updated: 20101020
  * License: MIT
- * Version: 1.2
+ * Version: 1.2.1
  *
  * URL:
  * http://javascript.nwbox.com/ContentLoaded/
@@ -16,35 +16,35 @@
 // @win window reference
 // @fn function reference
 function contentLoaded(win, fn) {
+  function init(e) {
+    var t = e.type;
+    if (t == 'readystatechange' && doc.readyState != 'complete') return;
+    (t == 'load' ? win : doc)[rem](pre + t, init, false);
+    if (!done++) fn.call(win, t || e);
+  }
 
-	var done = false, top = true,
+  function poll() {
+    try { root.doScroll('left'); } catch(e) { setTimeout(poll, 50); return; }
+    init('poll');
+  }
 
-	doc = win.document, root = doc.documentElement,
+  var doc = win.document
+   , root = doc.documentElement
+   , done = 0
+    , top = 1
+    , ael = 'addEventListener' in doc
+    , add = ael ? 'addEventListener' : 'attachEvent'
+    , rem = ael ? 'removeEventListener' : 'detachEvent'
+    , pre = ael ? '' : 'on';
 
-	add = doc.addEventListener ? 'addEventListener' : 'attachEvent',
-	rem = doc.addEventListener ? 'removeEventListener' : 'detachEvent',
-	pre = doc.addEventListener ? '' : 'on',
-
-	init = function(e) {
-		if (e.type == 'readystatechange' && doc.readyState != 'complete') return;
-		(e.type == 'load' ? win : doc)[rem](pre + e.type, init, false);
-		if (!done && (done = true)) fn.call(win, e.type || e);
-	},
-
-	poll = function() {
-		try { root.doScroll('left'); } catch(e) { setTimeout(poll, 50); return; }
-		init('poll');
-	};
-
-	if (doc.readyState == 'complete') fn.call(win, 'lazy');
-	else {
-		if (doc.createEventObject && root.doScroll) {
-			try { top = !win.frameElement; } catch(e) { }
-			if (top) poll();
-		}
-		doc[add](pre + 'DOMContentLoaded', init, false);
-		doc[add](pre + 'readystatechange', init, false);
-		win[add](pre + 'load', init, false);
-	}
-
+  if (doc.readyState == 'complete') fn.call(win, 'lazy');
+  else {
+    if (doc.createEventObject && root.doScroll) {
+      try { top = !win.frameElement; } catch(e) {}
+      if (top) poll();
+    }
+    doc[add](pre +'DOMContentLoaded', init, false);
+    doc[add](pre +'readystatechange', init, false);
+    win[add](pre +'load', init, false);
+  }
 }
